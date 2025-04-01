70528e7ea0dbb96a81a86b674fd4886dc4cfa189
Josué Mercadillo <josuemercally@outlook.com>
2025-04-01 14:04:56 -0600

    Added SignalR self host.

---
+++ ScannerAgent/ScannerHub.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace ScannerAgent
+{
+    public class ScannerHub : Hub
+    {
+        private ScannerManager scannerManager = new ScannerManager();
+
+        public string[] GetScanners()
+        {
+            return scannerManager.GetScanners().ToArray();
+        }
+
+        public string ScanDocument(string scannerName)
+        {
+            string filePath = scannerManager.Scan(scannerName);
+            Clients.All.SendAsync("DocumentScanned", filePath);
+            return filePath;
+        }
+    }
+}
---
+++ ScannerAgent/SignalRServer.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace ScannerAgent;
+
+internal class SignalRServer
+{
+    private IHost _host;
+
+    public void Start()
+    {
+        _host = CreateHostBuilder().Build();
+        _host.RunAsync();
+        Console.WriteLine("Servidor SignalR iniciado en http://localhost:5001");
+    }
+
+    public void Stop()
+    {
+        _host?.StopAsync().Wait();
+    }
+
+    public static IHostBuilder CreateHostBuilder() =>
+        Host.CreateDefaultBuilder()
+            .ConfigureServices(services =>
+            {
+                services.AddSignalR();
+            })
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseUrls("http://localhost:5001");
+                webBuilder.Configure(app =>
+                {
+                    app.UseRouting();
+                    app.UseEndpoints(endpoints =>
+                    {
+                        endpoints.MapHub<ScannerHub>("/scannerHub");
+                    });
+                });
+            });
+}
