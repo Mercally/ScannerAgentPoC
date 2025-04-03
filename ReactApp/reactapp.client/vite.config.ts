c956fa6e75dfa638422676a4c9371d260338660b
Josué Mercadillo <josuemercally@gmail.com>
2025-04-03 09:57:14 -0600

    Update vite.config.ts

--- ReactApp/reactapp.client/vite.config.ts
+++ ReactApp/reactapp.client/vite.config.ts
@@ -55,10 +55,10 @@ export default defineConfig({
                 secure: false
             }
         },
-        port: parseInt(env.DEV_SERVER_PORT || '53348')//,
-        //https: isCI ? undefined : {
-            //key: fs.readFileSync(keyFilePath),
-            //cert: fs.readFileSync(certFilePath),
-        //}
+        port: parseInt(env.DEV_SERVER_PORT || '53348'),
+        https: {
+            key: fs.readFileSync(keyFilePath),
+            cert: fs.readFileSync(certFilePath),
+        }
     }
 })
